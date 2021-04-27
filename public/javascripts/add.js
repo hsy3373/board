934e2ab1ba0ae364652fbe4ad5f7ff99caa3b9e9
hsy3373 <79007439+hsy3373@users.noreply.github.com>
2021-04-27 17:50:28 +0900

    first commit

---
+++ public/javascripts/add.js
@@ -0,0 +1,12 @@
+const titleInput = document.querySelector(".input.title-input");
+
+const contentValue = {
+  title: "",
+  writer: "",
+  content: "",
+  password: ""
+};
+
+function clickAdd() {
+  titleInput.addEventListener("submit", )
+}
